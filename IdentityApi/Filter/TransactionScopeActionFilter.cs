66b74533af2e2c7ea04c42c3318c2d645ee3ce04
Administrator <Administrator@DC001>
2022-07-13 23:09:50 +0800

    Action筛选器

--- IdentityApi/Filter/TransactionScopeActionFilter.cs
+++ IdentityApi/Filter/TransactionScopeActionFilter.cs
@@ -1,6 +1,34 @@
-﻿namespace IdentityApi.Filter
+﻿using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Reflection;
+using System.Transactions;
+
+namespace IdentityApi.Filter
 {
-    public class TransactionScopeActionFilter:IAsync
+    public class TransactionScopeActionFilter : IAsyncActionFilter
     {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+
+            bool hasNotTransactionScopeAttribute = false;
+            if (context.ActionDescriptor is ControllerActionDescriptor)
+            {
+                var actionDescriptor = (ControllerActionDescriptor)context.ActionDescriptor;
+                hasNotTransactionScopeAttribute = actionDescriptor.MethodInfo.IsDefined(typeof(NotTransactionScopeAttribute));
+
+                if (hasNotTransactionScopeAttribute)
+                {
+                    await next();
+                    return;
+                }
+
+                using TransactionScope scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+                var result = await next();
+                if (result.Exception == null)
+                {
+                    scope.Complete();
+                }
+            }
+        }
     }
 }
